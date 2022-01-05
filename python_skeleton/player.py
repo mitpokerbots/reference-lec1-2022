8f27e1b388c9870e6ad7065bbbfe104113de90cb
mattmcmanus41 <mattmcmanus41@gmail.com>
2022-01-04 20:59:25 -0500

    adding the additional python bot and renaming lecture bot to pair_all_in_bot

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -23,48 +23,7 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        
-        self.strong_hole = False #keep track of whether or not we have strong hole cards
-
-    def allocate_cards(self, my_cards):
-        ranks = {}
-
-
-
-        for card in my_cards:
-            card_rank  = card[0]
-            card_suit = card[1]
-
-
-            if card_rank in ranks:
-                ranks[card_rank].append(card)
-            else:
-                ranks[card_rank] = [card]
-
-        pairs = [] #keeps track of the pairs that we have 
-        singles = [] #other 
-
-        for rank in ranks:
-            cards = ranks[rank]
-
-            if len(cards) == 1: #single card, can't be in a pair
-                singles.append(cards[0])
-            
-            elif len(cards) == 2 or len(cards) == 4: #a single pair or two pairs can be made here, add them all
-                pairs += cards
-            
-            else: #len(cards) == 3  A single pair plus an extra can be made here
-                pairs.append(cards[0])
-                pairs.append(cards[1])
-                singles.append(cards[2])
-
-        if len(pairs) > 0: #we found a pair! update our state to say that this is a strong round
-            self.strong_hole = True
-        
-        allocation = pairs + singles 
         pass
-
-    
 
     def handle_new_round(self, game_state, round_state, active):
         '''
@@ -78,13 +37,12 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
-        game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
-        round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
-        my_cards = round_state.hands[active]  # your cards
-        big_blind = bool(active)  # True if you are the big blind
-
-        self.allocate_cards(my_cards) #allocate our cards to our board allocations
+        #my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        #game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        #round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        #my_cards = round_state.hands[active]  # your cards
+        #big_blind = bool(active)  # True if you are the big blind
+        pass
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
@@ -98,13 +56,12 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        my_delta = terminal_state.deltas[active]  # your bankroll change from this round
-        previous_state = terminal_state.previous_state  # RoundState before payoffs
-        street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
-        my_cards = previous_state.hands[active]  # your cards
-        opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
-        
-        self.strong_hole = False #reset our strong hole flag
+        #my_delta = terminal_state.deltas[active]  # your bankroll change from this round
+        #previous_state = terminal_state.previous_state  # RoundState before payoffs
+        #street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
+        #my_cards = previous_state.hands[active]  # your cards
+        #opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        pass
 
     def get_action(self, game_state, round_state, active):
         '''
@@ -120,50 +77,23 @@ class Player(Bot):
         Your action.
         '''
         legal_actions = round_state.legal_actions()  # the actions you are allowed to take
-        street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
-        my_cards = round_state.hands[active]  # your cards
-        board_cards = round_state.deck[:street]  # the board cards
-        my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
-        opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
-        my_stack = round_state.stacks[active]  # the number of chips you have remaining
-        opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
-        continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
-        my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
-        opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
-        net_upper_raise_bound = round_state.raise_bounds()
-        stacks = [my_stack, opp_stack] #keep track of our stacks
-
-        net_cost = 0
-        my_action = None
-
-        if (RaiseAction in legal_actions and self.strong_hole): #only consider raising if the hand we have is strong
-            min_raise, max_raise = round_state.raise_bounds()
-            max_cost = max_raise - my_pip
-
-
-            if max_cost <= my_stack - net_cost: #make sure the max_cost is something we can afford! Must have at least this much left after our other costs
-                my_action = RaiseAction(max_raise) #GO ALL IN!!!
-                net_cost += max_cost
-            
-            elif CallAction in legal_actions[i]: # check-call
-                my_action = CallAction()
-                net_cost += continue_cost
-
-            else:
-                my_action = CheckAction()
-        elif CheckAction in legal_actions: #if we can call, do so
-            my_action = CheckAction()
-        else:
-            my_action = CallAction()
-            net_cost += continue_cost #add the cost of the continue to our net cost
-
-
-        return my_action
-
-
-
-
-        
+        #street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
+        #my_cards = round_state.hands[active]  # your cards
+        #board_cards = round_state.deck[:street]  # the board cards
+        #my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
+        #opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
+        #my_stack = round_state.stacks[active]  # the number of chips you have remaining
+        #opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
+        #continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
+        #my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
+        #opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+        #if RaiseAction in legal_actions:
+        #    min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+        #    min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+        #    max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+        if CheckAction in legal_actions:  # check-call
+            return CheckAction()
+        return CallAction()
 
 
 if __name__ == '__main__':
